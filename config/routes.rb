2619aabfee87c9c4a4f9d2759139567dd880ff45
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-02-18 12:06:09 +0100

    Watch list - Root path created with Movies index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :movies, only: [:index, :show]
+  root to: "movies#index"
+  resources :movies, only: [:show]
   resources :lists, only: [:index, :show, :new, :create, :destroy]
   resources :bookmarks, only: [:new, :create, :destroy]
 end
